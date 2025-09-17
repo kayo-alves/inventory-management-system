9907cfcec4fdde8f130fb43514e90f0138b293d3
Kayo Alves <kayosilva577@gmail.com>
2025-09-17 17:51:45 -0300

    fix: regex sintax

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -14,7 +14,7 @@ class AuthController {
             }
 
             // email format validation
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,}$/;
             if (!emailRegex.test(email)) {
                 return res.status(400).json({
                 success: false,
